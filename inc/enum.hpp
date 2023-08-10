0eb49d22dedc49fd0b4ff1eee908e55eebcd85ec
pbremondFR <87093237+pbremondFR@users.noreply.github.com>
2023-08-10 23:43:38 +0200

    cleanup implementation, tweak examples

--- inc/enum.hpp
+++ inc/enum.hpp
@@ -6,6 +6,63 @@
 namespace const_enum
 {
 
+using namespace std::string_view_literals;
+
+namespace detail
+{
+
+	/*
+	* When this function gets templated with a valid number casted to the enum, its signature looks something
+	* like "bool isEnumMemberValid<enumType, enumMemberName>()". But when it's invocated with an
+	* invalid number casted to the enum, it looks like "bool isEnumMemberValid<enumType, (enumType)42>()".
+	* If there's a parenthesis, your enum does not contain this integer!
+	*
+	* So, calling this function with an arbitrary int, like isEnumMemberValid<MyEnum, (MyEnum)42>() tells
+	* you whether your enum contains the value 42 or not.
+	* This technique is used along with parameter pack expansion in the code below.
+	*/
+	template <typename E, E EnumMember>
+	consteval bool	is_enum_member_valid()
+	{
+		constexpr std::string_view	funcsig = __FUNCSIG__;
+		constexpr size_t			start = funcsig.find(',', funcsig.rfind('<')) + 1;
+		static_assert(start != std::string_view::npos);
+		return funcsig[start] != '(';
+	}
+
+	/*
+	* Using the same signature trick as above, this extracts the enum member's name at compile-time.
+	* Only working with string_view allows easy compile-time work.
+	*/
+	template <typename E, E EnumMember>
+	consteval std::string_view	get_enum_member_name()
+	{
+		constexpr std::string_view	funcsig = __FUNCSIG__;
+		constexpr size_t			start = funcsig.find(',', funcsig.rfind('<')) + 1;
+		constexpr size_t			end = funcsig.rfind('>');
+		static_assert(start != std::string_view::npos && end != std::string_view::npos);
+
+		if (funcsig[start] == '(')
+			return ""sv;
+		else
+			return std::string_view(&funcsig[start], end - start);	// Using iterators here fucks things up for some reason. Some sort of constexpr stack/step limit?
+	}
+
+	// From https://stackoverflow.com/questions/35625079/offset-for-variadic-template-integer-sequence
+	template <std::size_t Offset, std::size_t ... Is>
+	constexpr std::index_sequence<(Offset + Is)...> add_offset(std::index_sequence<Is...>)
+	{
+		return {};
+	}
+
+	template <std::size_t Offset, std::size_t N>
+	constexpr auto make_offset_index_sequence()
+	{
+		return add_offset<Offset>(std::make_index_sequence<N>{});
+	}
+
+}	// namespace detail
+
 /*
 * The functions below allow you to extract enum member name/value pairs, at compile time.
 * For example, if you have an enum, and want a map of that enum with a string of the member
@@ -13,78 +70,66 @@ namespace const_enum
 *
 * This is really cool, it essentially gives you information that's only available at compile-time,
 * which you can copy and use at runtime!
+* 
+* Using an index_sequence and parameter pack expansion (https://en.cppreference.com/w/cpp/language/parameter_pack),
+* get_enum_array() returns an array containing the enum's string representation of all integers
+* in the given sequence. If an index is invalid, the string is empty. Otherwise, the string
+* contains the enum member's name.
 */
 
-using namespace std::string_view_literals;
 
 /*
-* When this function gets templated with a valid number casted to the enum, its signature looks something
-* like "bool isEnumMemberValid<enumType, enumMemberName>()". But when it's invocated with an
-* invalid number casted to the enum, it looks like "bool isEnumMemberValid<enumType, (enumType)42>()".
-* If there's a parenthesis, your enum does not contain this integer!
-*
-* So, calling this function with an arbitrary int, like isEnumMemberValid<MyEnum, (MyEnum)42>() tells
-* you whether your enum contains the value 42 or not.
-* This technique is used along with parameter pack expansion in the code below.
+* Tests whether or not the given integer matches a valid enum member.
 */
-template <typename E, E EnumMember>
+template <typename E, int EnumMember>
 consteval bool	is_enum_member_valid()
 {
-	constexpr std::string_view	funcsig = __FUNCSIG__;
-	constexpr size_t			start = funcsig.find(',', funcsig.rfind('<')) + 1;
-	static_assert(start != std::string_view::npos);
-	return funcsig[start] != '(';
+	return detail::is_enum_member_valid<E, (E)EnumMember>();
 }
 
 /*
-* Using the same signature trick as above, this extracts the enum member's name at compile-time.
-* Only working with string_view allows easy compile-time work.
+* Gets a string representation of the enum member matching given integer. If no such
+* member exists, returns an empty string.
 */
-template <typename E, E EnumMember>
+template <typename E, int EnumMember>
 consteval std::string_view	get_enum_member_name()
 {
-	constexpr std::string_view	funcsig = __FUNCSIG__;
-	constexpr size_t			start = funcsig.find(',', funcsig.rfind('<')) + 1;
-	constexpr size_t			end = funcsig.rfind('>');
-	static_assert(start != std::string_view::npos && end != std::string_view::npos);
-
-	if (funcsig[start] == '(')
-		return ""sv;
-	else
-		return std::string_view(&funcsig[start], end - start);	// Using iterators here fucks things up for some reason. Some sort of constexpr stack/step limit?
+	return detail::get_enum_member_name<E, (E)EnumMember>();
 }
 
 /*
-* Using an index_sequence and parameter pack expansion (https://en.cppreference.com/w/cpp/language/parameter_pack),
-* this returns an array containing the enum representation of all numbers in the given sequence.
-* If an index is invalid, the string is empty. Otherwise, the string contains the enum member's name.
+* Returns an array containing the string representation of all enum members in the given
+* integer sequence. If a member does not correspond to an integer, its string representation
+* is an empty string.
 */
-template <typename E, size_t N, size_t... Is>
-consteval std::array<std::string_view, N>	get_enum_array(std::index_sequence<Is...>)
+template <typename E, size_t... Is>
+consteval auto	get_enum_array(std::index_sequence<Is...> sequence)
 {
-	constexpr std::array<std::string_view, N>	array = {
-		get_enum_member_name<E, (E)Is>()...
+	constexpr std::array<std::string_view, sequence.size()>	array = {
+		detail::get_enum_member_name<E, (E)Is>()...
 	};
 	return array;
 }
 
+/*
+* Returns an array containing the string representation of all enum members up to N.
+* If a member does not correspond to an integer, its string representation is an empty string.
+*/
 template <typename E, size_t N>
 consteval std::array<std::string_view, N>	get_enum_array()
 {
-	return get_enum_array<E, N>(std::make_index_sequence<N>{});
+	return get_enum_array<E>(std::make_index_sequence<N>{});
 }
 
-// From https://stackoverflow.com/questions/35625079/offset-for-variadic-template-integer-sequence
-template <std::size_t Offset, std::size_t ... Is>
-constexpr std::index_sequence<(Offset + Is)...> add_offset(std::index_sequence<Is...>)
+/*
+* Returns an array containing the string representation of all enum members in the range [Start;End).
+* If a member does not correspond to an integer, its string representation is an empty string.
+*/
+template <typename E, size_t Start, size_t End>
+consteval auto	get_enum_array_range()
 {
-	return {};
-}
-
-template <std::size_t Offset, std::size_t N>
-constexpr auto make_offset_index_sequence()
-{
-	return add_offset<Offset>(std::make_index_sequence<N>{});
+	static_assert(Start <= End, "Invalid range given to " __FUNCTION__);
+	return get_enum_array<E>(detail::make_offset_index_sequence<Start, End - Start>());
 }
 
 }	// namespace constexpr enum
--- src/ConstexprEnumShenanigans.cpp
+++ src/ConstexprEnumShenanigans.cpp
@@ -14,7 +14,6 @@ enum e_example {
 
 int main()
 {
-	std::cout << "get_enum_array:\n";
 	/*
 	* You can get an array matching your enum, containing all enum names as string_views.
 	* If there are values not contained in your enum, they're replaced by an empty string.
@@ -23,29 +22,33 @@ int main()
 	for (auto const &i : names_array)
 		std::cout << std::format("Enum value is: '{}'\n", i);
 
-	std::cout << "\nget_enum_array with offset:\n";
 	/*
-	* If you want to start your array with an offset in the enum, add an offset in the
-	* std::index_sequence with this helper function:
+	* You can also just take a range in the enum instead of the whole thing:
 	*/
-	constexpr auto offset_names_array = const_enum::get_enum_array<e_example, 3>(
-		const_enum::make_offset_index_sequence<FOOBAR, 3>());
-	for (auto const &i : offset_names_array)
+	constexpr size_t offset = 6;
+	constexpr auto ranged_names_array = const_enum::get_enum_array_range<e_example, offset, SIZEOF_ENUM>();
+	for (auto const &i : ranged_names_array)
 		std::cout << std::format("Enum value is: '{}' (from offset)\n", i);
 
-	std::cout << "\nget_enum_member_name:\n";
 	/*
 	* Or simply get the string representation of any integer you'd like.
 	* Once again, empty string means no matching enum value.
 	*/
 	constexpr int check_me = 7;
-	constexpr auto name = const_enum::get_enum_member_name<e_example, (e_example)check_me>();
+	constexpr auto name = const_enum::get_enum_member_name<e_example, check_me>();
 	std::cout << std::format("Integer {} matches enum member '{}'\n", check_me, name);
 
-	std::cout << "\nis_enum_member_valid:\n";
 	/*
 	* If you just want to check if a certain value has a matching enum member, use this:
 	*/
-	constexpr bool has_member = const_enum::is_enum_member_valid<e_example, (e_example)4>();
-	std::cout << std::format("Enum member valid: {}", has_member);
+	constexpr bool has_member = const_enum::is_enum_member_valid<e_example, 4>();
+	std::cout << std::format("Enum member valid: {}\n", has_member);
+
+	/*
+	* Once you have determined enum values at compile-time, it's very easy to get what
+	* you want at runtime, just index the array. If you use a library with constexpr maps,
+	* you could even make it so you have a constexpr map of values.
+	*/
+	for (int i = 0; i < SIZEOF_ENUM; ++i)
+		std::cout << std::format("At runtime, {} corresponds to '{}'\n", i, names_array[i]);
 }
